2ddfd0f3f84cd4228266c365052a522919dabf58
fabcre <fabien@await.fr>
2020-02-03 13:57:10 +0100

    feat(git-help): start hotfix process

---
+++ src/data/hotfix.ts
@@ -0,0 +1,19 @@
+const hotfixSteps: string[] = [
+  'checkout sur le commit de prod',
+  'créer une branche à partir de la derniere version en prod, nom de la branche sous cette forme hotfix/letrucfixé',
+  'commit',
+  'via le système de déploiement (deployit) déployer la branhce en uat puis vérifier avant la mise en prod',
+  'via deploy it mettre en prod cette nouvelle branche comprennant la prod actuelle plus le commit de fix',
+  'a la suite du hotfix, mettre le commit sur la branche master en faisant un cherry pick depuis master du hotfix pour avoir la branche master a jour avec ce hotfix inclus',
+];
+
+export default hotfixSteps;
+
+export function getHotFixStepContent(step: number): string {
+  switch (step) {
+    case 0:
+      return 'checkout sur le commit de prod';
+    default:
+      return 'Etape inconnue';
+  }
+}
