48116c1936de2a48370eb7e8d510747d0b641678
fabcre <fabien@await.fr>
2020-02-08 15:38:37 +0100

    feat(git-help): add hotfix linear stepper

---
+++ src/Component/HotfixVerticalLInearStepper.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import hotfixSteps, { getHotfixStepContent } from '../data/hotfix';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      margin: '1em',
+    },
+    button: {
+      fontSize: 'smaller',
+      padding: '1px 7px',
+      marginTop: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+    actionsContainer: {
+      fontSize: 'smaller',
+      padding: '1px 7px',
+      marginBottom: theme.spacing(2),
+    },
+    resetContainer: {
+      fontSize: 'smaller',
+      padding: theme.spacing(3),
+    },
+  }),
+);
+
+const HotfixVerticalLinearStepper: React.FC = () => {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {hotfixSteps.map((label, index) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+            <StepContent>
+              <Typography>{getHotfixStepContent(index)}</Typography>
+              <div className={classes.actionsContainer}>
+                <div>
+                  <Button
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    className={classes.button}
+                  >
+                    Précédent
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {activeStep === hotfixSteps.length - 1
+                      ? 'Terminé'
+                      : 'Suivant'}
+                  </Button>
+                </div>
+              </div>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === hotfixSteps.length && (
+        <Paper square elevation={0} className={classes.resetContainer}>
+          <Typography>Hotfix Terminé!</Typography>
+          <Button onClick={handleReset} className={classes.button}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+    </div>
+  );
+};
+
+export default HotfixVerticalLinearStepper;
